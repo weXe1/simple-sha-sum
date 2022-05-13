52eb15ee9b71094a89c664f8564ea3b89703f8d6
weXe1 <80769613+weXe1@users.noreply.github.com>
2022-05-13 09:19:46 +0000

    added actual HMAC

--- hmac.pl
+++ hmac.pl
@@ -4,14 +4,15 @@ use warnings;
 use Digest::SHA;
 use Getopt::Long;
 
-my ($alg, $pass, $file);
+my ($alg, $key, $file, $hmac);
 
-my $usage = "\$ perl $0 -f <file> [-a <1|224|256|384|512>] [-p <password>]\n";
+my $usage = "\$ perl $0 -f <file> [-hmac] [-a <1|224|256|384|512>] [-k <key>]\n";
 
 GetOptions(
     'a|algorithm=i' => \$alg,
-    'p|password=s' => \$pass,
-    'f|file=s' => \$file
+    'k|key=s' => \$key,
+    'f|file=s' => \$file,
+    'h|hmac' => \$hmac
 );
 
 unless (defined $file) {
@@ -26,8 +27,8 @@ unless ($alg =~ /1|224|256|384|512/) {
     die $usage;
 }
 
-unless (defined $pass) {
-    $pass = '';
+unless (defined $key) {
+    $key = '';
 }
 
 my $data;
@@ -39,10 +40,18 @@ my $data;
     close $fh;
 }
 
-$data .= $pass;
+if ($hmac) {
+    my $func;
+    eval '$func = \&Digest::SHA::hmac_sha'.$alg.'_hex';
 
-my $sha = Digest::SHA->new($alg);
-$sha->add($data);
+    my $mac = &$func($data, $key);
 
-my $digest = $sha->hexdigest;
-print "$digest $file\n";
+    print "HMAC (SHA$alg) = $mac $file\n";
+}
+else {
+    my $sha = Digest::SHA->new($alg);
+    $sha->add($data);
+
+    my $digest = $sha->hexdigest;
+    print "SHA$alg = $digest $file\n";
+}
